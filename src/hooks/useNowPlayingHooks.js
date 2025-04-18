2f239a22f08cc5bf20828c4abec8f33cf24827e6
Aarzoo Trehan <aarzoo.trehan@agoda.com>
2025-04-19 00:09:11 +0530

    Removing the hardcode secret.

--- src/hooks/useNowPlayingHooks.js
+++ src/hooks/useNowPlayingHooks.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 const useNowPlayingHooks = () => {
     const dispatch = useDispatch();
   const [movieList, setMovieList] = useState([]);
+  const token = process.env.TMDB_TOKEN;
 
   const fetchData = async () => {
     const url = MOVIE_URL;
@@ -15,7 +16,7 @@ const useNowPlayingHooks = () => {
       headers: {
         accept: "application/json",
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMjVlNTQxYjUxNjQ5ZDQ5ZWY2NmRmNTcwMWYzYzAwZCIsIm5iZiI6MTc0NDk2MTIxMi4wNjMsInN1YiI6IjY4MDFmZWJjZTQ3NTM0MjVlZmFkMjgwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lfkREJirWXLqVwkhSXYjV6A53WTh-vK4f9ibJolSFUs",
+          `Bearer ${token}`,
       },
     };
     const data = await fetch(url, options);
